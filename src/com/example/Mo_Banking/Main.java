daab142ead44389cd258c141337078aa69e5e295
manu <manuel@toshiba.(none)>
2013-04-21 13:15:24 +0200

    main menu intents working, basic interface for new account

--- src/com/example/Mo_Banking/Main.java
+++ src/com/example/Mo_Banking/Main.java
@@ -1,15 +1,42 @@
 package com.example.Mo_Banking;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity
 {
+    private Button newAccountButton;
+    private Button seeStatusButton;
+    private Intent newViewIntent;
+    private Intent statusViewIntent;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        newAccountButton = (Button) findViewById(R.id.bt_main_new);
+        seeStatusButton  = (Button) findViewById(R.id.bt_main_status);
+
+        newAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newViewIntent = new Intent(getApplicationContext(),NewAccountView.class);
+                startActivity(newViewIntent);
+            }
+        });
+
+        seeStatusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(statusViewIntent);
+                statusViewIntent = new Intent(getApplicationContext(), SeeStatusView.class);
+            }
+        });
     }
 }
---
+++ src/com/example/Mo_Banking/SeeStatusView.java
@@ -0,0 +1,20 @@
+package com.example.Mo_Banking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: manuel
+ * Date: 4/21/13
+ * Time: 12:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SeeStatusView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seestatusview);
+
+    }
+}
